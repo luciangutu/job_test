2c42a76185be39b9999b572b49e74bb24f82ba8d
Lucian Gutu <lucian@climb.ro>
2019-10-09 21:56:22 +0300

    my solution to narcissistic problem
    https://www.codewars.com/kata/does-my-number-look-big-in-this/train/python

---
+++ narcissistic.py
@@ -0,0 +1,12 @@
+def narcissistic( value ):
+    mylist = list(str(value))
+    r = 0
+    c = 0
+    for i in mylist:
+        if i.isdigit():
+            c = int(i)**len(mylist)
+        r += c
+    return True if r == value else False
+
+print(narcissistic(7))
+print(narcissistic(371))
