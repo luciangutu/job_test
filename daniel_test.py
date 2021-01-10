83ab73d3f4514d71943a5d6fc626894cb0fe6645
Lucian Gutu <lucian@climb.ro>
2021-01-10 19:55:19 +0200

    Update daniel_test.py

--- daniel_test.py
+++ daniel_test.py
@@ -1,3 +1,8 @@
+"""
+Job interview test
+
+Having a random list: strings = ['gigi', 'duru', 'ionel', 'florescu', 'popovici', 'restaurant', 'ovidiu'] Get the number of 3 char groups from it: { "tau": 1, "ran": 1, "res": 2, "pop": 1, "gig": 1, "ion": 1, "flo": 1, "diu": 1, "dur": 1, "ovi": 2 }
+"""
 import json
 strings = ['gigi', 'duru', 'ionel', 'florescu', 'popovici', 'restaurant', 'ovidiu']
 result = []
